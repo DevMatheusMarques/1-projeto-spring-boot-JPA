d67e166ed74b9e875ae4b9beea5f123bdb9457e4
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-09-03 16:14:48 -0300

    feat: Created H2 database, test profile, JPA

--- src/main/java/com/projeto1SpringBoot/projeto1/entities/User.java
+++ src/main/java/com/projeto1SpringBoot/projeto1/entities/User.java
@@ -1,10 +1,18 @@
 package com.projeto1SpringBoot.projeto1.entities;
 
+
+
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String phone;
@@ -12,7 +20,7 @@ public class User implements Serializable {
 
     public User() {}
 
-    public User(Integer id, String name, String email, String phone, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -20,11 +28,11 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/com/projeto1SpringBoot/projeto1/resources/UserResource.java
+++ src/main/java/com/projeto1SpringBoot/projeto1/resources/UserResource.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1,"Matheus", "matheus@gmail.com", "18 981886018", "teste123");
-        return ResponseEntity.ok().body(user);
-    }
+//    @GetMapping
+//    public ResponseEntity<User> findAll() {
+//        User user = new User(1L,"Matheus", "matheus@gmail.com", "18 981886018", "teste123");
+//        return ResponseEntity.ok().body(user);
+//    }
 }
